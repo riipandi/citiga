7c3974d78c05ae5c4eff6b4475ad39ad44ca4cda
Aris Ripandi <ar.is@outlook.com>
2018-01-24 12:56:56 +0700

    Add helper tanggal format Indonesia

--- protected/application/helpers/dante_helper.php
+++ protected/application/helpers/dante_helper.php
@@ -9,6 +9,40 @@ if(! function_exists('dante_helper'))
 		$ci->output->set_status_header($statusHeader);
 		$ci->output->set_output(json_encode($response));
     }
+
+    function tanggal_indo($tanggal, $cetak_hari = false)
+    {
+        $hari = array ( 1 =>    'Senin',
+                    'Selasa',
+                    'Rabu',
+                    'Kamis',
+                    'Jumat',
+                    'Sabtu',
+                    'Minggu'
+                );
+                
+        $bulan = array (1 =>   'Januari',
+                    'Februari',
+                    'Maret',
+                    'April',
+                    'Mei',
+                    'Juni',
+                    'Juli',
+                    'Agustus',
+                    'September',
+                    'Oktober',
+                    'November',
+                    'Desember'
+                );
+        $split 	  = explode('-', $tanggal);
+        $tgl_indo = $split[2] . ' ' . $bulan[ (int)$split[1] ] . ' ' . $split[0];
+        
+        if ($cetak_hari) {
+            $num = date('N', strtotime($tanggal));
+            return $hari[$num] . ', ' . $tgl_indo;
+        }
+        return $tgl_indo;
+    }
     
 	function rupiah($val)
     {
