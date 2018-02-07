df4a7f0dba64d2a740bfd76ff5ddf9ad8367687f
Aris Ripandi <ar.is@outlook.com>
2018-02-07 12:53:16 +0700

    Delete idea folder

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if(!file_exists(dirname(__FILE__).'/protected/vendor')) {
     require_once 'protected/config.inc.php';
     if (defined(DB_NAME) || DB_NAME !== '') {
         if (!$conn = mysqli_connect(DB_HOST.':'.DB_PORT, DB_USER, DB_PASS, DB_NAME)) {
-            die('Please check your database connection parameter!');
+            die('Please check your databas e connection parameter!');
         }
     }
     require_once 'protected/system.php';
